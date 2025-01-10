33689635bf6bb418cd177f49460f8a4873b2b144
Joao Palharini <joao@palharini.me>
2025-01-10 15:41:51 -0300

    kubernetes: add k9s

--- modules/kubernetes.nix
+++ modules/kubernetes.nix
@@ -5,69 +5,33 @@ let
     pods-fzf = "kubectl get pods -A | fzf --print0 --header-lines 1 -n 2";
     pod-vars = "${aliases.pods-fzf} | read -r ns pod _rest";
   };
-
-  scripts = {
-    mkubed = (pkgs.writeShellScriptBin "mkubed" ''
-      tmpdir="/tmp/mkubed"
-
-      function start () {
-        mkdir -p $tmpdir
-        
-        minikube start \
-          --kubernetes-version=v1.29.9 \
-          --driver=vfkit \
-          --container-runtime=docker \
-          --cpus=6 \
-          --memory=6g
-
-        minikube addons enable ingress
-
-        eval $(minikube docker-env)
-
-        # nohup minikube mount $NU_HOME:/mnt/nu-repos > $tmpdir/mnt_nu_repos.log 2>&1 &
-        # echo $! > $tmpdir/mnt_nu_repos.pid
-
-        # nohup minikube mount $HOME/.m2:/mnt/m2-repo > $tmpdir/mnt_m2.log 2>&1 &
-        # echo $! > $tmpdir/mnt_m2.pid
-      }
-
-      function stop() {
-        echo "Stopping Minikube..."
-        rm -rf $tmpdir
-        minikube stop
-        
-        unset DOCKER_TLS_VERIFY DOCKER_HOST DOCKER_CERT_PATH MINIKUBE_ACTIVE_DOCKERD
-      }
-
-      function restart() {
-        stop
-        start
-      }
-
-      function recreate() {
-        minikube delete
-        start
-      }
-
-      $1
-    '');
+  deps = {
+    k9s-catppuccin-theme = (builtins.fetchurl "https://raw.githubusercontent.com/catppuccin/k9s/refs/heads/main/dist/catppuccin-mocha.yaml");
   };
 in {
-  home.packages = [
-    scripts.mkubed
-    pkgs.vfkit
-  ];
-  
-  programs.zsh = {
-    shellAliases = {
-      k = "kubectl";
-      kdp = "${aliases.pod-vars}; kubectl describe pod -n \"$ns\" \"$pod\"";
-      kl = "${aliases.pod-vars}; kubectl logs -n \"$ns\" \"$pod\"";
-      klf = "${aliases.pod-vars}; kubectl logs -n \"$ns\" \"$pod\" -f";
-      kpc = "${aliases.pod-vars}; echo -n \"$pod\" | pbcopy";
-      kssh = "${aliases.pod-vars}; kubectl exec -it -n \"$ns\" \"$pod\" -- /bin/bash";
-      mk = "minikube kubectl --";
-      mkube = "minikube";
+  home.file.".config/k9s/skins/catppuccin-mocha.yaml".source = deps.k9s-catppuccin-theme;
+
+  programs = {
+    zsh = {
+      shellAliases = {
+        k = "kubectl";
+        kdp = "${aliases.pod-vars}; kubectl describe pod -n \"$ns\" \"$pod\"";
+        kl = "${aliases.pod-vars}; kubectl logs -n \"$ns\" \"$pod\"";
+        klf = "${aliases.pod-vars}; kubectl logs -n \"$ns\" \"$pod\" -f";
+        kpc = "${aliases.pod-vars}; echo -n \"$pod\" | pbcopy";
+        kssh = "${aliases.pod-vars}; kubectl exec -it -n \"$ns\" \"$pod\" -- /bin/bash";
+        mk = "minikube kubectl --";
+        mkube = "minikube";
+      };
+    };
+    k9s = {
+      enable = true;
+      settings.k9s.ui.skin = "catppuccin-mocha";
+      aliases.aliases = {
+        pp = "v1/pods";
+        dep = "apps/v1/deployments";
+      };
     };
   };
+
 }
