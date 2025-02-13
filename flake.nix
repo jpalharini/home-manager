3d4b2edbd86460c22e1e9f0b0e06c538c8168a31
Joao Palharini <joao@palharini.me>
2025-02-13 13:32:12 -0300

    Started moving config to flakes

--- flake.nix
+++ flake.nix
@@ -3,23 +3,72 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-joao.url = "github:jpalharini/nixpkgs";
+    nixpkgs-joao.url = "github:jpalharini/nixpkgs/bump-espanso";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    #secrets.url = "path:./secrets";
   };
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-joao, nix-darwin, home-manager, ... }: {
-    darwinConfigurations."Joaos-MacBook-Pro-2" = nix-darwin.lib.darwinSystem {
-      modules = [
-        ./darwin.nix
-        home-manager.darwinModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users."joao.palharini" = import ./home-manager/macbook.nix;
-        }
-      ];
+  outputs = { 
+    self, nixpkgs, nixpkgs-joao, nix-darwin, home-manager, ... 
+  } @ inputs: 
+  
+    let
+      inherit (self) outputs;
+
+      mkNixosConfiguration = args: nixpkgs.lib.nixosSystem {
+        system = args.system;
+        modules = args.modules;
+      };
+
+      pkgsForSystem = system: import nixpkgs {
+        inherit system;
+      };
+
+      mkHomeConfiguration = args: home-manager.lib.homeManagerConfiguration {
+        pkgs = pkgsForSystem args.system;
+        extraSpecialArgs = {
+          pkgs-joao = import nixpkgs-joao {
+            system = args.system;
+            config.allowUnfree = true;
+          };
+        };
+        modules = args.modules;
+      };
+
+    in {
+      # todo: enable use of this in nu-precision
+      nixosConfigurations = {
+        nu-precision = mkNixosConfiguration {
+          system = "x86_64-linux";
+          modules = [
+            ./configuration.nix
+          ];
+        };
+      };
+
+      darwinConfigurations."Joaos-MacBook-Pro-2" = nix-darwin.lib.darwinSystem {
+        modules = [
+          ./darwin/configuration.nix
+        ];
+      };
+
+      homeConfigurations = {
+        "joao.palharini@joao.palharini" = mkHomeConfiguration {
+          system = "aarch64-darwin";
+          modules = [
+            ./home-manager/macbook.nix
+          ];
+        };
+
+        "jpalharini@nu-precision" = mkHomeConfiguration {
+          system = "x86_64-linux";
+          modules = [
+            /home-manager/linux.nix
+          ];
+        };
+      };
     };
-  };
 }
