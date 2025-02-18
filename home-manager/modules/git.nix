4daf6dd8df5fafb80cd3dca05672b4c4ac01ad30
Joao Palharini <joao@palharini.me>
2025-02-18 11:24:58 -0300

    hm/git: refactoring

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -1,40 +1,45 @@
 { config, pkgs, ... }:
 
 {
-  programs.git = {
-    enable = true;
-    userName = "Joao Palharini";
-    userEmail = "joao@palharini.me";
-    aliases = {
-      co = "checkout";
-      nb = "checkout -b";
-      p = "pull";
-      P = "push";
-    };
-    lfs = {
+  programs = {
+    git = {
       enable = true;
-      skipSmudge = true;
+      userName = "Joao Palharini";
+      userEmail = "joao@palharini.me";
+      aliases = {
+        co = "checkout";
+        nb = "checkout -b";
+        p = "pull";
+        P = "push";
+      };
+      lfs = {
+        enable = true;
+        skipSmudge = true;
+      };
+      signing = {
+        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDjJgxosQs2YuD1zrCtlO8iWhmtRd8T7cHFQLo57zPKt";
+        signByDefault = true;
+      };
+      extraConfig = {
+        init.defaultBranch = "main";
+        push.autoSetupRemote = true;
+        core.fsMonitor = true;
+        gpg.format = "ssh";
+      };
     };
-    signing = {
-      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDjJgxosQs2YuD1zrCtlO8iWhmtRd8T7cHFQLo57zPKt";
-      signByDefault = true;
-    };
-    extraConfig = {
-      init.defaultBranch = "main";
-      push.autoSetupRemote = true;
-      core.fsMonitor = true;
-      gpg.format = "ssh";
-    };
+    lazygit.enable = true;
   };
-
-  programs.lazygit.enable = true;
   
   home.file = {
-    ".config/lazygit/themes/catppuccin-mocha-mauve.yml".source = (builtins.fetchurl "https://raw.githubusercontent.com/catppuccin/lazygit/refs/heads/main/themes-mergable/mocha/mauve.yml");
+    ".config/lazygit/themes/catppuccin-mocha-mauve.yml".source = (builtins.fetchurl {
+      url = "https://raw.githubusercontent.com/catppuccin/lazygit/refs/heads/main/themes-mergable/mocha/mauve.yml";
+      sha256 = "19plm6zg2xs0bimbbcf3bq122qdhydxjn2z147n2mbshylnkz40i";
+    });
   };
 
   home.packages = with pkgs; [
     bfg-repo-cleaner
+    git-crypt
   ];
 
   programs.zsh.shellAliases = {
